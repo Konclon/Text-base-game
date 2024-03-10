ddb96d4e2e56d318e797b40b6d5afd45485e9c10
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-03-10 17:40:03 +0800

    removed intruction comments

--- twoBar/movesets.py
+++ twoBar/movesets.py
@@ -1,9 +1,4 @@
-# Stuff you must include
-# name
-# moves (arr [1,2,3])
-# player_health
-# enemy_health
-# moves 1,2,3
+# where [add to caster, add to opponent, name of skill]
 
 set1 = {
     "name":"set1",
@@ -13,4 +8,14 @@ set1 = {
     1:[0,-10,"Zap"],
     2:[-10,-20,"Lightning Strike"],
     3:[15,0,"Heal Hacks"]
+}
+
+set2 = {
+    "name":"set2",
+    "moves":[1,2,3],
+    "player_health":200,
+    "enemy_health":200,
+    1:[0,-20,"Small jab"]
+    2:[-100,-120,"Big die"]
+    3,[35,-20,"Life steal"]
 }
